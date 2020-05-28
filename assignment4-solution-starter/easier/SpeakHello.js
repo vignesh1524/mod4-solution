682609c4c84e5e8bac5f8df3deff83a32777f428
vignesh1524 <43825542+vignesh1524@users.noreply.github.com>
2020-05-28 19:29:01 +0530

    Update SpeakHello.js
    method name added

--- assignment4-solution-starter/easier/SpeakHello.js
+++ assignment4-solution-starter/easier/SpeakHello.js
@@ -8,20 +8,20 @@
  var helloSpeaker = {};
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello";
+
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 helloSpeaker.speak = function(name) {
   console.log(speakWord + " " + name);
-}
-
+};
+var speakWord = "Hello";
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
-window.helloSpeaker;
+window.helloSpeaker = helloSpeaker;
   
 }) (window);
